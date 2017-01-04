54a734b037131cdc237a17221a11e9429de0df3c
Kunihiko Miyoshi <miyoshik@gmail.com>
2017-01-04 18:15:59 +0900

    OSRMのビルドスクリプトの依存ライブラリを追加

--- data/D03_openstreetmap/build_osrm.sh
+++ data/D03_openstreetmap/build_osrm.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+sudo apt-get install -y cmake
+sudo apt-get install -y libboost-all-dev libtbb-dev liblua5.2-dev libluabind-dev libstxxl-dev libxml2 libxml2-dev libosmpbf-dev libbz2-dev libprotobuf-dev
 wget https://github.com/Project-OSRM/osrm-backend/archive/v5.1.0.zip
 unzip v5.1.0.zip
 cd osrm-backend-5.1.0
@@ -7,5 +9,5 @@ mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build .
-sudo cmake --build . --target install
+#sudo cmake --build . --target install
 
