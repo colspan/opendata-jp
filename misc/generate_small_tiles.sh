c0a418fcf05d2c0dd2b2421d689389ed573a4c9b
Kunihiko Miyoshi <miyoshik@gmail.com>
2017-01-26 20:43:11 +0900

    パスを修正

--- misc/generate_small_tiles.sh
+++ misc/generate_small_tiles.sh
@@ -2,4 +2,4 @@
 
 inputdir=$1
 outputdir=$2
-/home/kuni/local/src/tiletool-master/tiletool -z 10 -b 10/907-927/366-383 -i $inputdir -o $outputdir -e $inputdir/10/907/367.png
+/home/kuni/Dropbox/Tools/tiletool-master/tiletool -z 10 -b 10/907-927/366-383 -i $inputdir -o $outputdir -e $inputdir/10/907/367.png
